3466b902b783155315ac398acc24398f679fa965
listik <bas.bas.8181@mail.ru>
2024-12-25 15:48:46 +0300

    four

---
+++ task3.js
@@ -0,0 +1,71 @@
+//5.1.
+//1.
+/*
+const max = 5;
+
+//2.
+const randomNumber = Math.floor(Math.random() * max) + 1;
+console.log("Полученное число:", randomNumber);
+
+//3.
+let isGuessed = false;
+
+//4.
+while (!isGuessed) {
+    const userInput = prompt(`Угадайте число от 1 до ${maxValue}:`);
+    const userGuess = parseInt(userInput, 10);
+
+    if (userGuess === randomNumber) {
+        alert("Поздравляем! Вы угадали число!");
+        isGuessed = true; 
+        break;
+    } else if (userGuess < randomNumber) {
+        alert("Загаданное число больше. Попробуйте ещё раз.");
+    } else if (userGuess > randomNumber) {
+        alert("Загаданное число меньше. Попробуйте ещё раз.");
+    } else {
+        alert("Введите корректное число.");
+    }
+}*/
+
+//5.2.
+/*
+// 1.
+let counter = 0;
+
+//2.
+const step = 5;
+
+//3.
+do {
+    console.log("Текущее значение счетчика:", counter);
+    counter += step;
+} while (counter < 100);
+
+//4.
+console.log("Цикл завершен. Финальное значение счетчика:", counter);*/
+
+//5.3
+
+/*//1.
+const myWork = [];
+
+//2.
+for (let i = 1; i <= 10; i++) {
+//3.
+    const status = (i % 2 === 0) ? false : true;
+
+//4.
+    const lesson = {
+        name: `Lesson ${i}`,
+        status: status
+    };
+
+//5.
+    myWork.push(lesson);
+}
+
+//6.
+console.log(myWork);*/
+
+//5.4.
